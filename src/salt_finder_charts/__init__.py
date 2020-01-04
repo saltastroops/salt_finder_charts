37e8be421cf31a81d5bf21b354b92bd26dc8a363
Christian Hettlage <hettlage@saao.ac.za>
2020-01-04 18:36:09 +0200

    bumped version number

--- src/salt_finder_charts/__init__.py
+++ src/salt_finder_charts/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for SALT Finder Charts."""
 
 __author__ = """SAAO/SALT"""
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from salt_finder_charts.finder_charts import finder_charts
 from salt_finder_charts.standard_finder_charts import standard_finder_charts
