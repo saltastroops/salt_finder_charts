f325b37d69fc9883ffe29c832d027726a8fcb9cf
Christian Hettlage <hettlage@saao.ac.za>
2020-05-28 22:56:03 +0200

    Bumped version number

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/saltastroops/salt_finder_charts',
-    version='0.2.0',
+    version='0.3.2',
     zip_safe=False,
     entry_points={
         "console_scripts": [
--- src/salt_finder_charts/__init__.py
+++ src/salt_finder_charts/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for SALT Finder Charts."""
 
 __author__ = """SAAO/SALT"""
-__version__ = "0.2.0"
+__version__ = "0.3.2"
 
 from salt_finder_charts.finder_chart import finder_charts
 from salt_finder_charts.standard_finder_charts import standard_finder_charts
